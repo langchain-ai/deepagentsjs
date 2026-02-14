ec49f83c422f3edd7a67b185cb106557b4bcbbdf
Hunter Lovell <hunter@hntrl.io>
2026-02-13 22:18:49 -0800

    chore: fork wasmer-js glue layer into libs/wasix

---
+++ libs/wasix/rust/wasmer-js/src/net.rs
@@ -0,0 +1,3 @@
+// Removed: WebSocket-based remote networking (TCP/UDP via gateway).
+// We don't need TCP/UDP from WASM in our use case.
+// Original implementation used virtual_net::RemoteNetworkingClient over WebSocket.
---
+++ libs/wasix/rust/wasmer-js/src/ws.rs
@@ -0,0 +1,2 @@
+// Removed: WebSocket wrapper used by net.rs for remote networking.
+// Not needed since we removed the remote networking module.
