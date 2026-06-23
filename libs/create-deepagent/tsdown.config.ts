87846fe8685edb9565639cf221f0adea8ada8cd3
Alexander <aolsenjazz@gmail.com>
2026-06-23 15:52:26 -0400

    Add js to outExtensions, ensure compile to .js (rather than .mjs)

--- libs/create-deepagent/tsdown.config.ts
+++ libs/create-deepagent/tsdown.config.ts
@@ -8,6 +8,7 @@ export default defineConfig([
     clean: true,
     sourcemap: true,
     outDir: "dist",
+    outExtensions: () => ({ js: ".js" }),
     // Add shebang for executable
     banner: {
       js: "#!/usr/bin/env node",
